fa8991b96ca1f6930075abdc814a350dbdc9a789
Moaninglisa <78330179+Moaninglisa@users.noreply.github.com>
2023-06-28 22:17:01 -0700

    errorhandler

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const appointmentsRouter = require('./app/routes/appointmentRoutes');
-const errorHandlerMiddleware = require('./app/middleware/errorHandler');
 const db = require('./lib/database');
-const appointmentMiddleware = require('./app/middleware/appointmentMiddleware')
+const errorHandlerMiddleware = require('./middleware/errorHandler');
+
 
 const port = 3000;
 const app = express();
@@ -13,13 +13,13 @@ app.use(bodyParser.json());
 app.use('/api/v1/appointments', appointmentsRouter);
 
 // Error handlers always go last
-app.use(appointmentMiddleware());
 app.use(errorHandlerMiddleware());
+
 
 const dbConfig = {
   hostname: '127.0.0.1',
   port: 27017,
-  database: 'arca',
+  database: 'appointments',
   options: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
